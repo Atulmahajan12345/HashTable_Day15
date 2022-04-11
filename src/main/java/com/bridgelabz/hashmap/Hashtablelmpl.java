b27a50f0a0001a58630eeeedbcb2c3675af257b4
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-04-11 15:39:14 +0530

    FrequencyOfWordinSentence

--- src/main/java/com/bridgelabz/hashmap/Hashtablelmpl.java
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.hashmap;
-
-public class Hashtablelmpl {
-
-}
--- src/main/java/com/bridgelabz/hashmap/MyMapNode.java
+++ src/main/java/com/bridgelabz/hashmap/MyMapNode.java
@@ -1,5 +1,48 @@
 package com.bridgelabz.hashmap;
 
-public class MyMapNode {
+public class MyMapNode<T1, T2> {
+	T1 key;
+    T2 value;
+    MyMapNode<T1, T2> next;
 
+    public MyMapNode (T1 key, T2 value) {
+        this.key = key;
+        this.value = value;
+        next = null;
+    }
+
+    public T1 getKey() {
+        return key;
+    }
+
+    public void setKey(T1 key) {
+        this.key = key;
+    }
+
+    public T2 getValue() {
+        return this.value;
+    }
+
+    public void setValue(T2 value) {
+        this.value = value;
+    }
+
+    public MyMapNode<T1, T2> getNext() {
+        return next;
+    }
+
+    public void setNext(MyMapNode<T1, T2> next) {
+        this.next = (MyMapNode<T1, T2>) next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder nodeString = new StringBuilder();
+        nodeString.append("Node{" + "T1=").append(key).append(" T2=").append(value).append("}");
+        if(next != null)
+            nodeString.append("->").append(next);
+        return nodeString.toString();
+    }
 }
+
+
